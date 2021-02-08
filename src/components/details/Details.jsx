e56a692178fde1b7708da37a8fcbc6b6d33016a6
dl-watson <dlarkinwatson@gmail.com>
2021-02-08 15:01:42 -0800

    add Details to Router, starting to convert into a function component

---
+++ src/components/details/Details.jsx
@@ -0,0 +1,11 @@
+import React, { useState } from "react";
+
+const Details = () => {
+  const [character, setCharacter] = useState([]);
+
+  return <div></div>;
+};
+
+Details.propTypes = {};
+
+export default Details;
