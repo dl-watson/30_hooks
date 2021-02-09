77bd6a2a943fae1089e246d43a9aee7a00d9c18f
dl-watson <dlarkinwatson@gmail.com>
2021-02-08 17:03:33 -0800

    left align because fancy

--- src/components/details/ItemDetails.jsx
+++ src/components/details/ItemDetails.jsx
@@ -8,6 +8,13 @@ const ItemDetails = ({ character }) => {
     <div className={styles.ItemDetails}>
       {character.name}
       <img src={character.image} />
+      <div className={styles.details}>
+        <p>Quote: {character.quote}</p>
+        <p>Personality: {character.personality}</p>
+        <p>Birthday: {character.birthday}</p>
+        <p>Species: {character.species}</p>
+        <p>Phrase: {character.phrase}</p>
+      </div>
     </div>
   );
 };
